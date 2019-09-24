c41e1c71a43e23849d36ffc475755a9e5039ce5c
田中優太 <yuta.tanaka@justsystems.com>
2019-09-24 13:06:39 +0900

    spec5: IDを追加

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import dictionary from '../resources/dictionary'
  * エントリポイント
  */
 export default function main() {
-    dictionary.forEach((word) => {
-        console.log(word);
+    dictionary.forEach((word, index) => {
+        console.log(`${index + 1} : ${word}`);
     });
 }
